5ef7b6170f4384c10f19593746e3a1186eecb277
phamduykhanh2000 <87484578+phamduykhanh2000@users.noreply.github.com>
2021-12-12 20:59:34 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Ting Ting...!',
+    introDesc: `Bạn có một phần quà được gửi từ một thằng lạ mặt nào nó`,
+    btnIntro: 'MỞ',
+    title: 'Giáng Sinh là dịp để thể hiện tình iu🥰',
+    desc: 'Còn yêu hay không thì phải hỏi😙 ',
+    btnYes: 'Điên! Click vào để ra <33',
+    btnNo: 'Cái thằng khùng! :3',
+    question:'Tại sao nút [X] không bấm .Đồ điên <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tao yêu mầy <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Đêm giáng sinh anh qua rước nhá.🥰',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com/pdk.nhox' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
